efa68b0a861bda9a841e4a09b80fb391aa2beba8
karamchand11 <0215k.c@gmail.com>
2017-01-10 10:03:54 +0000

    update

---
+++ a.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+a=10
+if [ $a -gt 9 ]
+then
+echo "A is grater" 
+else 
+echo "a is smaller"
+exit 1
+fi
+
---
+++ b.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+b=20
+if [ $b -gt 9 ]
+then
+echo "b is grater" 
+else 
+echo "b is smaller"
+exit 1
+fi
+
